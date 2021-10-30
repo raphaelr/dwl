743c5d00eb9b93d0ed836c0a207a1df061b36a56
Raphael Robatsch <raphael-git@tapesoftware.net>
2021-10-30 16:09:07 +0200

    split wayland-ipc patch

--- quilt2git.sh
+++ quilt2git.sh
@@ -2,23 +2,42 @@
 # creates a git branch for every patch
 set -e
 from=upstream/main
-patches="tag-handling master-on-right-toggle wlr_virtual_pointer_v1 wayland-ipc"
-tmp="$(mktemp)"
+tmp=$(mktemp)
+trap 'rm "$tmp"' EXIT
+
+status="$(git status -s)"
+if [ -n "$status" ]; then
+	echo "working directory is not clean!"
+	git status
+	exit 1;
+fi
 
 headerof() {
-    awk '/---/ { exit; } { print; }' "$1"
+	awk '/---/ { exit; } { print; }' "$1"
 }
 
-for patch in $patches; do
-    fn="patches/${patch}.patch"
-    echo "$fn"
-    cp -f "$fn" "$tmp"
-    git branch -f "$patch" "$from"
-    git checkout "$patch"
-    patch -p1 --no-backup-if-mismatch -r - < "$tmp"
-    make -B
-    git add -A
-    headerof "$tmp" | git commit -F -
-    git reset --hard
-    git checkout master
-done
+series() {
+	name="$1"
+	echo "$name"
+	git branch -f "$name" "$from"
+	git checkout "$name"
+	while shift && [ -n "$1" ]; do
+		patch="$1"
+		git show "master:patches/$patch.patch" > "$tmp"
+		patch -p1 --no-backup-if-mismatch -r - < "$tmp"
+		make -B
+		git add -A
+		headerof "$tmp" | git commit -F -
+	done
+	git reset --hard
+	git checkout master
+}
+
+single() {
+	series "$1" "$1"
+}
+
+single tag-handling
+single master-on-right-toggle
+single wlr_virtual_pointer_v1
+series wayland-ipc wayland-ipc-protocol wayland-ipc-control
